bd9cd12ba2d2498495b439a52a117582d263ff51
Ian <ianburrows.sa@gmail.com>
2017-09-20 22:42:53 +0200

    Beginning

---
+++ Polymorphism/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Circle : Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a circle...");
+        }
+    }
+}
